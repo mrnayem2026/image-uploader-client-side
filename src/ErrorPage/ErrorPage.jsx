ee70539218e742862fb5897338a6c7c374a09adf
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-05-24 22:17:20 -0700

    Error Page style done

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import { Link, useRouteError } from 'react-router-dom'
+import errorImg from '../assets/404page.png'
 
 const ErrorPage = () => {
-    return (
-        <div>
-                I am form Error Page
+  const { error} = useRouteError()
+  return (
+    <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
+      <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+        <img src={errorImg} alt="ErrorImg" className='w-3/6'/>
+        
+        <div className='max-w-md text-center'>
+          <p className='text-2xl font-semibold md:text-3xl mb-8'>
+            {error?.message}
+          </p>
+          <Link
+            to='/'
+            className='btn btn-outline btn-primary text-3xl font-BebasNeue px-10'
+          >
+            Back to homepage
+          </Link>
         </div>
-    );
-};
+      </div>
+    </section>
+  )
+}
 
-export default ErrorPage;
+export default ErrorPage
