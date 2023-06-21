f7dba85d87ee7109a247fedddcbac908e79a4306
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-06-21 11:08:59 -0700

    All Image show done

--- src/Pages/All Image/AllImage.jsx
+++ src/Pages/All Image/AllImage.jsx
@@ -1,30 +1,66 @@
+import { useQuery } from "@tanstack/react-query";
 import React, { useState, useEffect } from "react";
+import useAxiosSecure from "../../customHooks/useAxiosSecure";
+import { HashLoader } from "react-spinners";
+import Swal from "sweetalert2";
 const AllImage = () => {
-  const [imageData, setImageData] = useState(null);
+  const [axiosSecure] = useAxiosSecure();
+  const [copiedUrl, setCopiedUrl] = useState('');
 
-  useEffect(() => {
-    fetch("http://localhost:5000/image")
-      .then((response) => response.json())
-      .then((data) => {
-        setImageData(data);
+  const {
+    data: images = [],
+    refetch,
+    isLoading,
+  } = useQuery(["images"], async () => {
+    const res = await axiosSecure.get(`/uploade_images`);
+    return res.data;
+  });
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <HashLoader color="#A6ADBA" />
+      </div>
+    );
+  }
+
+  const copyToClipboard = (url) => {
+    navigator.clipboard.writeText(url)
+      .then(() => {
+        setCopiedUrl(url);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Copy Image url successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
       })
       .catch((error) => {
-        console.error("Error:", error);
+        console.error('Failed to copy URL to clipboard:', error);
       });
-  }, []);
+  };
 
   return (
-    <div>
-      {imageData ? (
-        <div>
-          <h1>Image Display</h1>
-          <img
-            src={`data:${imageData.contentType};base64,${imageData.data}`}
-            alt="Uploaded"
-          />
-          <p>Filename: {imageData.filename}</p>
-          <p>Size: {imageData.size} bytes</p>
-        </div>
+    <div className="container mx-auto py-14 grid lg:grid-cols-3 gap-9">
+      {images ? (
+        images.map((img) => (
+          <div className="card w-96 bg-base-100 shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)]">
+            <figure>
+              <img
+                src={img.image}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body flex-grow-0 py-5">
+              <h2 className="card-title">Image URL</h2>
+              <p className="p-4 bg-base-300 text-white rounded-xl">{img.image}</p>
+              <div className=" pt-4">
+                <button className="btn btn-primary w-full " onClick={() => copyToClipboard(img.image)}>Copy URL</button>
+              </div>
+            </div>
+          </div>
+        ))
       ) : (
         <p>No image found.</p>
       )}
